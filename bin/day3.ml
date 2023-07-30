f720cd56b5abe7e9fcb4fc25083fb2e30957124d
Scott-fo <scottfo@live.com>
2023-07-30 13:26:42 +0100

    feat: day3.2

--- bin/day3.ml
+++ bin/day3.ml
@@ -33,7 +33,27 @@ let read_lines file =
 let score_rucksack line =
   Rucksack.create line |> Rucksack.find_duplicate |> Score.get_score
 
+let three_rucksacks ~f list =
+  let rec aux acc f = function
+    | a :: b :: c :: tl -> aux (acc + f a b c) f tl
+    | _ -> acc
+  in
+  aux 0 f list
+
+let check_three_rucksacks a b c =
+  let set_a = Items.of_string a in
+  let set_b = Items.of_string c in
+  let set_c = Items.of_string b in
+  Items.inter set_a set_b |> Items.inter set_c |> Items.choose
+  |> Score.get_score
+
 let () =
+  (* Part 1 *)
   read_lines "./input/day3.txt"
   |> List.map ~f:score_rucksack |> List.fold ~init:0 ~f:( + )
-  |> Stdio.printf "Total score: %d\n"
+  |> Stdio.printf "Part 1 Total score: %d\n";
+
+  (* Part 2 *)
+  read_lines "./input/day3.txt"
+  |> three_rucksacks ~f:check_three_rucksacks
+  |> Stdio.printf "Part 2 Total score: %d\n"
